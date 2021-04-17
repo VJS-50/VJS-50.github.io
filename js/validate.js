aca8f94fe9f3d3262fed59b2b73ab4fd36064f56
VJS-50 <yu.vincent349@gmail.com>
2021-04-17 18:01:33 -0400

    organized the code

--- js/validate.js
+++ js/validate.js
@@ -2,41 +2,39 @@ function validate(event) {
   // TODO - write custom validation logic to validate the longitude and latitude
   // values. The latitude value must be a number between -90 and 90; the
   // longitude value must be a number between -180 and 180. If either/both are
-  // invalid, show the appropriate error message in the form, and stop the 
+  // invalid, show the appropriate error message in the form, and stop the
   // form from being submitted. If both values are valid, allow the form to be
   // submitted.
   //console.log('TODO - validate the longitude, latitude values before submitting');
 
   var form = document.getElementById("addObsForm");
 
-
   var lat = form["lat"].value;
   var lng = form["lng"].value;
-  
+
   var latValid, lngValid;
 
-
-  if (lat < -90 || 90 < lat || isNaN(lat)=== true ) {
+  if (lat < -90 || 90 < lat || isNaN(lat) === true) {
     latValid = false;
     document.getElementById("latInvalid").style.display = "inline";
-  }else (latValid = true)
+  } else latValid = true;
 
-  if (lng < -180 || 180 < lng || isNaN(lng)=== true) {
+  if (lng < -180 || 180 < lng || isNaN(lng) === true) {
     lngValid = false;
     document.getElementById("lngInvalid").style.display = "inline";
-  }else (lngValid = true)
+  } else lngValid = true;
 
   if (latValid == true && lngValid == true) {
     document.getElementById("latInvalid").style.display = "none";
     document.getElementById("lngInvalid").style.display = "none";
     return true;
-  }else {
+  } else {
     return false;
   }
 }
 
 // Wait for the window to load, then set up the submit event handler for the form.
-window.onload = function() {
-  const form = document.querySelector('form');
+window.onload = function () {
+  const form = document.querySelector("form");
   form.onsubmit = validate;
 };
